c209ab4ec2706d1dc38131b8a93d3b411b75b2ef
Senje <ro2xq@yandex.ru>
2022-12-03 22:07:15 +1000

    finnaly for customer

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -19,8 +19,8 @@ $(document).ready(() => {
             $('.background').removeClass('background_small');
             $('.header__pagename').html('');
             $('.main-menu__item a').css('pointer-events', 'none');
+            $('.content').removeClass('active');
             setTimeout(() => {
-                $('.content').removeClass('active');
                 $('.main-menu__item a').removeAttr('style');
             }, 1000);
         });
@@ -43,7 +43,70 @@ $(document).ready(() => {
                 $('.nav').addClass('nav_hide');
                 // change background to small
                 $('.background').addClass('background_small');
+                // change phone mask
+                phoneMask();
+                // slider
+                sliders();
             }
         });
     }
+
+    // sliders
+    function sliders() {
+        $('#slider-menu, #slider-breakfast, #slider-bar').slick({
+            arrows: false,
+            dots: true,
+        });
+    }
+    // Mask for phone inputs
+    function phoneMask() {
+        const prefixNumber = (str) => {
+            if (str === "7") {
+                return "7 (";
+            }
+            if (str === "8") {
+                return "8 (";
+            }
+            if (str === "9") {
+                return "7 (9";
+            }
+            return "7 (";
+        };
+        // ======================================
+        $('.input-phone').on("input", function () {
+            const value = this.value.replace(/\D+/g, "");
+            const numberLength = 11;
+
+            let result;
+            if (this.value.includes("+8") || this.value[0] === "8") {
+                result = "";
+            } else {
+                result = "+";
+            }
+
+            //
+            for (let i = 0; i < value.length && i < numberLength; i++) {
+                switch (i) {
+                    case 0:
+                        result += prefixNumber(value[i]);
+                        continue;
+                    case 4:
+                        result += ") ";
+                        break;
+                    case 7:
+                        result += "-";
+                        break;
+                    case 9:
+                        result += "-";
+                        break;
+                    default:
+                        break;
+                }
+                result += value[i];
+            }
+            this.value = result;
+        });
+
+    }
+
 });
